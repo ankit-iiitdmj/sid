10eae1f8d5b4c9264b210c91c1747df23e7381d7
ankit-iiitdmj <50139544+ankit-iiitdmj@users.noreply.github.com>
2024-02-02 11:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def create_names_pdf(names):
     names = [name.upper() for name in names]  # Convert all names to uppercase
     # Constants for the page and name area
     page_width, page_height = A4[1], A4[0]  # Landscape orientation
-    name_area_width, name_area_height = 11 * inch, 1.8 * inch
-    margin_top, margin_left = 0.1 * inch, 0.1 * inch
+    name_area_width, name_area_height = 11 * inch, 1.95 * inch
+    margin_top, margin_left = 0.2 * inch, 0.4 * inch
     max_names_per_page = 4  # Maximum number of names per page
 
     # Create a bytes buffer for the PDF file
@@ -39,7 +39,7 @@ def create_names_pdf(names):
             y = page_height - margin_top - (index + 1) * name_area_height
             total_width = sum(c.stringWidth(char, bold_font, 12) for char in name)
             scale_x = name_area_width / total_width
-            scale_y = name_area_height / 12
+            scale_y = name_area_height / 8
             for char in name:
                 char_width = c.stringWidth(char, bold_font, 12)
                 c.saveState()
@@ -57,8 +57,8 @@ def create_names_pdf(names):
 # Function to create a PDF with numbers
 def create_numbers_pdf(numbers):
     page_width, page_height = A4[1], A4[0]
-    number_area_width, number_area_height = 5 * inch, 7 * inch
-    margin_top, margin_left = 0.01 * inch, 0.01 * inch
+    number_area_width, number_area_height = 5.9 * inch, 7.8 * inch
+    margin_top, margin_left = 0.1 * inch, 0.2 * inch
     max_numbers_per_page = 2
 
     # Create a bytes buffer for the PDF file
@@ -69,7 +69,7 @@ def create_numbers_pdf(numbers):
     for i, number in enumerate(numbers):
         if i % max_numbers_per_page == 0 and i != 0:
             c.showPage()
-        x = margin_left + (i % 2) * number_area_width
+        x = margin_left + (i % 2) * (number_area_width - 0.4 * inch)
         y = page_height - margin_top - number_area_height
 
         number_str = str(number)
@@ -77,7 +77,7 @@ def create_numbers_pdf(numbers):
         total_width = c.stringWidth(number_str, 'Teko-SemiBold', 12)
 
         scale_x = number_area_width / total_width
-        scale_y = number_area_height / (12 * 1.2)
+        scale_y = number_area_height / 8
 
         c.saveState()
         c.translate(x, y)
@@ -92,7 +92,7 @@ def create_numbers_pdf(numbers):
     return buffer
 
 # Streamlit UI
-st.title('PDF Generator for Names and Numbers')
+st.title('Name Number Generator')
 
 # Link to download sample CSV
 with open('sample.csv', 'r') as f:
